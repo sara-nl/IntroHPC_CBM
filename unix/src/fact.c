6af906223bc8e2ffa5551989032443a6fce65e0c
Marco Verdicchio <marco.verdicchio@surfsara.nl>
2018-08-30 23:59:10 +0200

    Added C example for advanced

---
+++ unix/src/fact.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	int n, i ;
+	unsigned long long factorial ;
+
+	// show error if no argument is passed
+	if (argc < 2)
+	{
+		printf("Error! At least one argument needed.");
+		return 2;
+	}
+
+	else
+	{
+		factorial = 1;
+		n = atoi(argv[1]);
+
+		// show error if the user enters a negative integer
+		if (n < 0)
+		{
+			printf("Error! Factorial of a negative number doesn't exist.");
+			return 2;
+		}
+
+		for(i=1; i <= n; ++i)
+		{
+			factorial *= i;              // factorial = factorial*i;
+		}
+
+		printf("Factorial of %d = %llu \n", n, factorial);
+	}
+
+	return 0;
+}
